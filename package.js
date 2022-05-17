8504791ec93e158544f125bfae655819650c7456
Will Reiske <wreiske@mieweb.com>
2022-05-17 13:30:38 -0700

    Bump C3

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-	name: 'perak:c3',
+	name: 'wreiske:c3',
 	summary: "Reactive C3 charting library based on D3",
-	version: "1.1.0",
-	git: "https://github.com/perak/meteor-c3.git"
+	version: "1.1.1",
+	git: "https://github.com/wreiske/meteor-c3.git"
 });
 
 // Before Meteor 0.9?
